7f7cfb411cfab1882c0fb3826a2429eeffcaf9de
Luke Kang <luk3kang@gmail.com>
2021-03-05 22:39:34 +0900

    Add local buffer on draw

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -1,10 +1,17 @@
-import React, { forwardRef, useImperativeHandle, useRef } from 'react'
+import React, {
+  CSSProperties,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+} from 'react'
 
 type CanvasMouseEvent = React.MouseEvent<HTMLCanvasElement, MouseEvent>
 
+export type Point = [number, number]
+
 export interface CanvasEvent {
   lineWidth: number
-  path: string
+  points: Point[]
 }
 
 export interface CanvasRef {
@@ -14,82 +21,137 @@ export interface CanvasRef {
 interface CanvasProps {
   width: number
   height: number
-  onDraw: (e: CanvasEvent) => void
+  style?: CSSProperties
+  onDraw?: (e: CanvasEvent) => void
 }
 
 interface CanvasState {
   isDrawing: boolean
-  prev?: CanvasMouseEvent
+  prev?: Point
 }
 
-const getXY = (surface: HTMLElement, e: CanvasMouseEvent) => [
-  e.clientX - surface.offsetLeft,
-  e.clientY - surface.offsetTop,
+const getPoint = (container: HTMLElement, e: CanvasMouseEvent): Point => [
+  e.clientX - container.offsetLeft,
+  e.clientY - container.offsetTop,
 ]
 
-const Canvas = forwardRef<CanvasRef, CanvasProps>(({ onDraw }, ref) => {
-  const targetRef = useRef<HTMLCanvasElement>(null)
-  const stateRef = useRef<CanvasState>({
-    isDrawing: false,
-  })
+const drawCanvas = (
+  canvasRef: React.RefObject<HTMLCanvasElement>,
+  e: CanvasEvent
+) => {
+  const target = canvasRef.current
+  if (!target) return
 
-  useImperativeHandle(
-    ref,
-    () => ({
-      draw(e) {
-        const target = targetRef.current
-        if (!target) return
+  const ctx = target.getContext('2d')
+  if (!ctx) return
+  ctx.lineWidth = e.lineWidth
+  const [sx, sy] = e.points[0]
+  const moves = e.points
+    .slice(1)
+    .map(([x, y]) => `L ${x} ${y}`)
+    .join('')
+  const path = `M ${sx} ${sy} ${moves}`
+  ctx.stroke(new Path2D(path))
+}
 
-        const ctx = target.getContext('2d')
-        if (!ctx) return
-        ctx.lineWidth = e.lineWidth
-        ctx.stroke(new Path2D(e.path))
-      },
-    }),
-    []
-  )
+const Canvas = forwardRef<CanvasRef, CanvasProps>(
+  ({ width, height, style, onDraw }, ref) => {
+    const containerRef = useRef<HTMLDivElement>(null)
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+    const tempCanvasRef = useRef<HTMLCanvasElement>(null)
+    const stateRef = useRef<CanvasState>({
+      isDrawing: false,
+    })
 
-  const handleMouseEvent = (e: CanvasMouseEvent) => {
-    const target = targetRef.current
-    if (!target) return
+    const draw = drawCanvas.bind(null, canvasRef)
+    const drawTempCanvas = drawCanvas.bind(null, tempCanvasRef)
+    const clearTempCanvas = () => {
+      if (!tempCanvasRef.current) return
 
-    const state = stateRef.current
-
-    switch (e.type) {
-      case 'mousedown':
-        state.isDrawing = true
-        break
-
-      case 'mouseup':
-        state.isDrawing = false
-        break
-
-      case 'mousemove':
-        if (state.isDrawing && state.prev) {
-          const [x1, y1] = getXY(target, state.prev)
-          const [x2, y2] = getXY(target, e)
-          const path = `M ${x1} ${y1} L ${x2} ${y2} Z`
-          onDraw({
-            lineWidth: 2,
-            path,
-          })
-        }
-        break
-
-      default:
-        break
+      const ctx = tempCanvasRef.current.getContext('2d')
+      ctx?.clearRect(0, 0, width, height)
     }
-    state.prev = e
-  }
 
-  return (
-    <canvas
-      onMouseDown={handleMouseEvent}
-      onMouseUp={handleMouseEvent}
-      onMouseMove={handleMouseEvent}
-      ref={targetRef}
-    />
-  )
-})
+    useImperativeHandle(
+      ref,
+      () => ({
+        draw(e) {
+          clearTempCanvas()
+          draw(e)
+        },
+      }),
+      []
+    )
+
+    const handleMouseEvent = onDraw
+      ? (e: CanvasMouseEvent) => {
+          const target = canvasRef.current
+          const container = containerRef.current
+          if (!target || !container) return
+
+          const state = stateRef.current
+
+          const p = getPoint(container, e)
+
+          switch (e.type) {
+            case 'mousedown':
+              state.isDrawing = true
+              onDraw({
+                lineWidth: 2,
+                points: [p],
+              })
+              break
+
+            case 'mouseup':
+              state.isDrawing = false
+              break
+
+            case 'mousemove':
+              if (state.isDrawing) {
+                onDraw({
+                  lineWidth: 2,
+                  points: [p],
+                })
+                if (state.prev) {
+                  drawTempCanvas({
+                    lineWidth: 2,
+                    points: [state.prev, p],
+                  })
+                }
+              }
+              break
+
+            default:
+              break
+          }
+
+          state.prev = p
+        }
+      : undefined
+
+    return (
+      <div style={{ position: 'relative' }} ref={containerRef}>
+        <canvas
+          width={width}
+          height={height}
+          style={style}
+          onMouseDown={handleMouseEvent}
+          onMouseUp={handleMouseEvent}
+          onMouseMove={handleMouseEvent}
+          ref={canvasRef}
+        />
+        <canvas
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            pointerEvents: 'none',
+          }}
+          ref={tempCanvasRef}
+        />
+      </div>
+    )
+  }
+)
 
 export default Canvas
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,33 +2,51 @@ import { useEffect, useRef, useState } from 'react'
 import io, { Socket } from 'socket.io-client'
 import Canvas, { CanvasEvent, CanvasRef } from '@components/Canvas'
 
+const TIMEOUT = 100
+
 const IndexPage = () => {
   const [socket, setSocket] = useState<typeof Socket>()
+  const bufferTimerRef = useRef<any>(null)
   const bufferRef = useRef<CanvasEvent[]>([])
   const canvasRef = useRef<CanvasRef>(null)
 
   useEffect(() => {
     // FIXME: Replace with constants (next.js runtime dotenv)
+    const canvas = canvasRef.current
+    if (!canvas) return
+
     const s = io('http://localhost:3001/')
     s.on('connect', () => {
       console.log(`socket connected: ${s.id}`)
       setSocket(s)
     })
-    s.on('draws', (draws: any) => {
-      draws.forEach((draw: any) => {
-        canvasRef.current?.draw(draw)
-      })
-    })
+    s.on('draws', canvas.draw)
     s.open()
   }, [setSocket])
 
+  const clearBuffer = () => {
+    if (!socket) return
+    const buffer = bufferRef.current
+    const merged = buffer.reduce(
+      (merged, item) => {
+        merged.points.push(...item.points)
+        return merged
+      },
+      { lineWidth: buffer[0].lineWidth, points: [] }
+    )
+    socket.emit('draws', merged)
+    bufferRef.current = []
+  }
+
   const handleDraw = (e: CanvasEvent) => {
     bufferRef.current.push(e)
-    if (!socket) return
-    if (bufferRef.current.length > 10) {
-      socket.emit('draws', bufferRef.current)
-      bufferRef.current = []
+    if (bufferTimerRef.current) {
+      clearTimeout(bufferTimerRef.current!)
     }
+    bufferTimerRef.current = setTimeout(() => {
+      clearBuffer()
+      bufferTimerRef.current = null
+    }, TIMEOUT)
   }
 
   return (
