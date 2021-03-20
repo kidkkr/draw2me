a2d889101686b60ef61da1a2fb579bf4dbd48fe8
Luke Kang <luk3kang@gmail.com>
2021-03-20 22:06:25 +0900

    Split page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,71 +1,8 @@
-import { useEffect, useRef, useState } from 'react'
-import io, { Socket } from 'socket.io-client'
-import { SOCKET_IO } from '../env'
-import Canvas, { CanvasEvent, CanvasRef } from '../components/Canvas'
-
-const TIMEOUT = 100
-
-const IndexPage = () => {
-  const [socket, setSocket] = useState<typeof Socket>()
-  const bufferTimerRef = useRef<any>(null)
-  const bufferRef = useRef<CanvasEvent[]>([])
-  const canvasRef = useRef<CanvasRef>(null)
-
-  useEffect(() => {
-    const canvas = canvasRef.current
-    if (!canvas) return
-
-    const socket = io(SOCKET_IO, {
-      secure: true,
-    })
-    socket.on('connect', () => {
-      setSocket(socket)
-    })
-    socket.on('draws', canvas.draw)
-    socket.open()
-  }, [setSocket])
-
-  const clearBuffer = () => {
-    if (!socket) return
-    const buffer = bufferRef.current
-    const merged = buffer.reduce(
-      (merged, item) => {
-        merged.points.push(...item.points)
-        return merged
-      },
-      { lineWidth: buffer[0].lineWidth, points: [] }
-    )
-    socket.emit('draws', merged)
-    bufferRef.current = []
-  }
-
-  const handleDraw = (e: CanvasEvent) => {
-    bufferRef.current.push(e)
-    if (bufferTimerRef.current) {
-      clearTimeout(bufferTimerRef.current!)
-    }
-    bufferTimerRef.current = setTimeout(() => {
-      clearBuffer()
-      bufferTimerRef.current = null
-    }, TIMEOUT)
-  }
-
-  return (
-    <main>
-      <h1>imgfly</h1>
-      <p>{socket ? `socket.id: ${socket.id}` : 'socket disconnected'}</p>
-      <Canvas
-        style={{
-          border: 'solid 2px grey',
-          boxSizing: 'border-box',
-        }}
-        width={350}
-        height={250}
-        onDraw={handleDraw}
-        ref={canvasRef}
-      />
-    </main>
-  )
-}
+const IndexPage = () => (
+  <main>
+    <h1>imgf.ly</h1>
+    <h2>WIP</h2>
+  </main>
+)
 
 export default IndexPage
